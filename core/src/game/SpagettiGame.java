9bf922154d9367293e06f76017b4126b1adddd4b
Alexandr Lopoukhov <tigraride@gmail.com>
2015-06-22 22:30:50 +0300

    init

---
+++ core/src/game/SpagettiGame.java
@@ -0,0 +1,12 @@
+package game;
+
+import com.badlogic.gdx.Game;
+
+public class SpagettiGame extends Game {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ desktop/src/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.SpagettiGame;
+
+public class DesktopLauncher {
+	public static void main(final String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 640;
+		config.height = 480;
+		new LwjglApplication(new SpagettiGame(), config);
+	}
+}
